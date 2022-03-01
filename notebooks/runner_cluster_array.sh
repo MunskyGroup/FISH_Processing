3407aec59a152e92a8682f6e6586c971f8a7f3f0
luisub <luisubald@gmail.com>
2022-02-28 21:29:11 -0700

    cluster parallel

---
+++ notebooks/runner_cluster_array.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+#SBATCH --job-name=FISH
+#SBATCH --output=out-%j.out
+#SBATCH --error=out-%j.err
+#SBATCH --partition=all
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --gres=gpu:1
+#SBATCH -a 0-8    # Including the last element 
+
+# module purge
+module load gnu9/9.4.0 
+module load cudnn/8.3-10.2
+
+list_ILB_Rep1=( \
+'smFISH_images/Linda_smFISH_images/Confocal/20220125/GAPDH-Cy3_NFKBIA-Cy5_WO_IL-1B' \
+'smFISH_images/Linda_smFISH_images/Confocal/20220203/GAPDH-Cy3_NFKBIA-Cy5_5min_10ng_mL_IL-1B' \
+'smFISH_images/Linda_smFISH_images/Confocal/20220127/GAPDH-Cy3_NFKBIA-Cy5_10min_10ng_mL_IL-1B' \
+'smFISH_images/Linda_smFISH_images/Confocal/20220125/GAPDH-Cy3_NFKBIA-Cy5_15min_10ng_mL_IL-1B' \
+'smFISH_images/Linda_smFISH_images/Confocal/20220125/GAPDH-Cy3_NFKBIA-Cy5_20min_10ng_mL_IL-1B' \
+'smFISH_images/Linda_smFISH_images/Confocal/20220124/GAPDH-Cy3_NFKBIA-Cy5_30min_10ng_mL_IL-1B' \
+'smFISH_images/Linda_smFISH_images/Confocal/20220124/GAPDH-Cy3_NFKBIA-Cy5_1h_10ng_mL_IL-1B' \
+'smFISH_images/Linda_smFISH_images/Confocal/20220124/GAPDH-Cy3_NFKBIA-Cy5_2h_10ng_mL_IL-1B' \
+'smFISH_images/Linda_smFISH_images/Confocal/20220124/GAPDH-Cy3_NFKBIA-Cy5_3h_10ng_mL_IL-1B' ) 
+
+send_data_to_NAS=1       # If data sent back to NAS use 1.
+diamter_nucleus=120      # approximate nucleus size in pixels
+diameter_cytosol=250     # approximate cytosol size in pixels
+psf_z=300                # Theoretical size of the PSF emitted by a [rna] spot in the z plan, in nanometers.
+psf_yx=105               # Theoretical size of the PSF emitted by a [rna] spot in the yx plan, in nanometers.
+nucleus_channel=0        # Channel to pass to python for nucleus segmentation
+cyto_channel=2           # Channel to pass to python for cytosol segmentation
+FISH_channel=1           # Channel to pass to python for spot detection
+FISH_second_channel=0    # Channel to pass to python for spot detection in a second Channel, if 0 is ignored.
+path_to_config_file="$HOME/FISH_Processing/config.yml"
+
+for idx in {0..8} # Including the last element 
+do
+     folder=${list_ILB_Rep1[idx]}
+     ~/.conda/envs/FISH_processing/bin/python ./pipeline_executable.py "$folder" $send_data_to_NAS $diamter_nucleus $diameter_cytosol $psf_z $psf_yx $nucleus_channel $cyto_channel $FISH_channel $FISH_second_channel "$path_to_config_file" 
+done
+
+
+# ########### TO EXECUTE RUN IN TERMINAL #########################
+# run as: sbatch runner_cluster_array.sh
+
+exit 0
+
+# ########### TO REMOVE SOME FILES #########################
+
+# To remove files
+# ls *.tif
+# rm -r temp_*
+# rm -r analysis_*
+# rm -r slurm* out* temp_* 
+
+# ########### SLURM COMMANDS #########################
+# scancel [jobid]
+# squeue -u [username]
+# squeue
+# sinfo
