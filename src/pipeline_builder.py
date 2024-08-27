a6781bd5fb78dcaa4748e70a323db0f9d6765a98
Formanj-0 <formanj@colostate.edu>
2024-08-27 13:27:38 -0600

    Create pipeline_builder.py

---
+++ src/pipeline_builder.py
@@ -0,0 +1,59 @@
+import pickle
+
+from src import Experiment, PipelineSettings, ScopeClass, Pycromanager2NativeDataType, PipelineDataClass, Pipeline, \
+    ConsolidateImageShapes, TrimZSlices, CalculateSharpness, AutomaticSpotDetection, CellSegmentationStepClass, \
+    SpotDetectionStepClass, SavePDFReport
+
+
+#%% User specified Stuff
+name = 'Luis_Standard_Pipeline'
+
+raw_data_location = r"C:\Users\Jack\Desktop\H128_Tiles_100ms_5mW_Blue_15x15_10z_05step_2"
+
+FISHChannel = [0]
+NucChannel = [0]
+
+voxel_size_z = 500
+
+
+#%% Pipeline
+# Settings
+settings = PipelineSettings()
+
+# Scope
+scope = ScopeClass()
+
+# Experiment
+experiment = Experiment(raw_data_location, voxel_size_z=voxel_size_z)
+
+# Bridge
+bridge = Pycromanager2NativeDataType
+
+# PrePipeline Steps
+preSteps = [ConsolidateImageShapes(), TrimZSlices(), CalculateSharpness(), AutomaticSpotDetection()]
+
+# Pipeline Steps
+Steps = [CellSegmentationStepClass(), SpotDetectionStepClass()]
+
+# PostPipeline Step
+postSteps = [SavePDFReport()]
+
+
+#%% Compile into a dictionary
+# Compile
+pipelineDict = {
+    'settings': settings,
+    'experiment': experiment,
+    'scope': scope,
+    'bridge': bridge,
+    'prepipeline_steps': preSteps,
+    'pipeline_steps': Steps,
+    'postpipeline_steps': postSteps,
+    'kwargs': {}
+}
+
+# Pickle
+with open(f'{name}.pkl', 'wb') as f:
+    pickle.dump(pipelineDict, f)
+
+
