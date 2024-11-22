459981acb7b112abf4926a35eac73075a75f3b40
Jack <155103214+Formanj-0@users.noreply.github.com>
2024-11-21 22:00:03 -0700

    running on cluster

---
+++ cluster/runner_pipeline.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#SBATCH --gres=gpu:4
+# #SBATCH --nodelist=gpu2    # gpu2 gpu3 gpu4
+#SBATCH --partition=all
+#SBATCH --ntasks=4
+#SBATCH --job-name=t2
+
+# module purge
+module load gnu9/9.4.0
+module load cudnn/8.3-10.2
+
+echo "Starting my job..."
+
+# Start timing the process
+start_time=$(date +%s)
+
+kwarg_location=$1
+path_to_executable="${PWD%/*}/src/pipeline_executable.py"
+
+# ########### PYTHON PROGRAM #############################
+
+output_names=""output__"${kwarg_location////__}"".txt"
+ ~PWD%/*/venv/Scripts/activate "$path_to_executable" "$kwarg_location" >> "$output_names" &
+ wait
+
+
+# End timing the process
+end_time=$(date +%s)
+total_time=$(( (end_time - start_time) / 60 ))
+
+# Print the time to complete the process
+echo "Total time to complete the job: $total_time minutes"
+
+# ########### TO EXECUTE RUN IN TERMINAL #########################
+# run as: sbatch runner_cluster.sh /dev/null 2>&1 & disown
+
+exit 0
+
+# ########### TO REMOVE SOME FILES #########################
+
+# To remove files
+# ls *.tif
+# rm -r temp_*
+# rm -r analysis_*
+# rm -r slurm* out* temp_* masks_*
+
+# ########### SLURM COMMANDS #########################
+# scancel [jobid]
+# squeue -u [username]
+# squeue
