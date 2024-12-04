aa7d56960ad9de0a649743649b007e6dac5f9a20
Formanj-0 <formanj@colostate.edu>
2024-12-03 23:40:07 -0700

    Preparing for major testing

--- src/Test/Test_Steps.py
+++ src/Test/Test_Steps.py
@@ -1,10 +1,24 @@
 import pytest
+import sys
+import os
+
+sys.path.append(os.path.join(os.getcwd(), '..'))
+
+from src.Parameters import Parameters, ScopeClass, Experiment, DataContainer, Settings
+
+# To Test: These are the most basic steps we will ever use so they must all work always.
+from src.GeneralStep import SequentialStepsClass, IndependentStepClass, FinalizingStepClass, StepClass
+from src.FinalizationSteps import Save_Images, Save_Masks, Save_Outputs, return_to_NAS, remove_local_data, remove_local_data_but_keep_h5
+from src.SequentialSteps import SimpleCellposeSegmentaion, BIGFISH_SpotDetection
+from src.IndependentSteps import FFF2NativeDataType, Pycromanager2NativeDataType, NativeDataType
 
 def test_sanity():
     # Setup
     # exercise
     # assert
     assert True == True
+
+
 
 
 
